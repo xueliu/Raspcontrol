eeb820d36b2b9e93c0e20541c1de05953f84da75
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-07-26 00:01:43 +0000

    Setup Enhancment

--- setup.php
+++ setup.php
@@ -14,8 +14,25 @@ if ($_POST['password']) {
         "password":	"' . $_POST['password'] .'"
     }';
     fwrite($fh, $stringData); 
-    header('location: index.php');
-} else {
+	
+	 $options['host'] = "fusionstrike.com"; 
+	 $options['port'] = 5984;
+	 $options['user'] = "raspcontrol";
+	 $options['pass'] = "aaaa1234";
+	
+	 $couch = new CouchSimple($options); 
+	
+	 	$resp = $couch->send("GET", "/raspcontrol/installCounter/"); 
+	 	$db = json_decode($resp);
+		$installs = $db->{'installs'};
+		$add = $installs + 1;
+	 	$resp = $couch->send("DELETE", "/raspcontrol/");
+		$resp = $couch->send("PUT", "/raspcontrol"); 
+		$arr = array('id' => "installCounter", 'installs' => "".$add."");
+		$json = json_encode($arr);
+		$resp = $couch->send("PUT", "/raspcontrol/installCounter", $json); 
+	    header('location: index.php');
+	} else {
 
 ?>
 
@@ -41,5 +58,48 @@ if ($_POST['password']) {
 </div>
 <?php require('app/includes/_footer.php'); ?>
 <?php
+}
+
+
+
+
+ class CouchSimple {
+    function CouchSimple($options) {
+       foreach($options AS $key => $value) {
+          $this->$key = $value;
+       }
+    } 
+   
+   function send($method, $url, $post_data = NULL) {
+      $s = fsockopen($this->host, $this->port, $errno, $errstr); 
+      if(!$s) {
+         echo "$errno: $errstr\n"; 
+         return false;
+      } 
+
+      $request = "$method $url HTTP/1.0\r\nHost: $this->host\r\n"; 
+
+      if ($this->user) {
+         $request .= "Authorization: Basic ".base64_encode("$this->user:$this->pass")."\r\n"; 
+      }
+
+      if($post_data) {
+         $request .= "Content-Length: ".strlen($post_data)."\r\n\r\n"; 
+         $request .= "$post_data\r\n";
+      } 
+      else {
+         $request .= "\r\n";
+      }
+
+      fwrite($s, $request); 
+      $response = ""; 
+
+      while(!feof($s)) {
+         $response .= fgets($s);
+      }
+
+      list($this->headers, $this->body) = explode("\r\n\r\n", $response); 
+      return $this->body;
+   }
 }
 ?>
