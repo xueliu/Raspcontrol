946a8890e4e4ff17ef5a46c8fe633e1fd0f169c4
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-07-26 08:34:32 +0000

    Added Tracking

--- app/includes/_header.php
+++ app/includes/_header.php
@@ -8,6 +8,21 @@ session_start();
 <head>
     <title>Raspcontrol - The Raspberry Pi Control Centre</title>
     <link rel="stylesheet" href="app/styles/style.css" type="text/css" media="screen" charset="utf-8">
+    
+    <script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-33662683-1']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>
+
 </head>
 
 <body>
