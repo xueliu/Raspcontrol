964c9b6f150b4f66ef0cd2f868e67880f1d8f24f
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-08-05 17:30:25 +0000

    Fixed update Raspcontrol Command

--- app/commands/_updateraspcontrol.php
+++ app/commands/_updateraspcontrol.php
@@ -5,11 +5,25 @@ if($_SESSION['username'] == ""){
 	die("You are not logged in");
 }
 
-echo '<pre>';
+if(isset($_POST['location'])){
+	echo '<pre>';
 
-$last_line = system('if [ ! -d /usr/lib/git-core ]; then sudo apt-get -y install git-core;fi && sudo git clone https://github.com/Bioshox/Raspcontrol.git /var/www/RaspcontrolUpdate && sudo rm -R -f /var/www/Raspcontrol && sudo mv /var/www/RaspcontrolUpdate /var/www/Raspcontrol', $retval);
+$last_line = system('if [ ! -d /usr/lib/git-core ]; then sudo apt-get -y install git-core;fi && sudo git clone https://github.com/Bioshox/Raspcontrol.git '.$_POST["location"].'Update && sudo rm -R -f '.$_POST["location"].' && sudo mv '.$_POST["location"].'Update '.$_POST["location"].'', $retval);
 // Printing additional info
 echo '
 </pre>'; ?>
 Raspcontrol updated!<br/>
 <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Return To Previous Page</a>
+<?php
+}else{
+?>
+
+Please enter the full path to where Raspcontrol is installed. (E,g: /var/www/Raspcontrol)<br/><br/>
+<form action="" method="post">
+	<input type="text" name="location">
+	<input type="submit" value="Update!">
+</form>
+
+<?php
+}
+?>
