ce57347d05ce566378bf93c560346fccd68b5657
Andrew <andrewspaterson@yahoo.com>
2012-08-05 16:04:59 +0100

    Added a Raspcontrol Update button
    This shows an update button on the menu bar along the top and sources
    the update from the .git.
    If git-core isn't installed then it does this before attempting a
    download.
    
    Due to rights access I had to download it to a temp directory
    (/var/www/RaspcontrolUpdate) before renaming to the original
    /var/www/Raspcontrol.
    
    Tested multiple times to the best of my ability and it works as expected
    on my machine, however due to me being very much a Linux beginner the
    permissions on /www/ may differ to that of a real user so I'd appreciate
    some double checking before merging. I don't forsee any issues, but as I
    say, my setup may be different than a normal user.
    
    I'm also learning to use github so sorry if these updates reach you in a
    way that don't make any sense...I still have more reading to do about
    branches/commits etc etc.
    
    I will be looking into making it so the button only appears when there's
    a valid update present, there's no point allowing users to use bandwidth
    to update Raspcontrol with an existing version. I'll look into this as a
    future update.

---
+++ app/commands/_updateraspcontrol.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if($_SESSION['username'] == ""){
+	die("You are not logged in");
+}
+
+echo '<pre>';
+
+$last_line = system('if [ ! -d /usr/lib/git-core ]; then sudo apt-get -y install git-core;fi && sudo git clone https://github.com/Bioshox/Raspcontrol.git /var/www/RaspcontrolUpdate && sudo rm -R -f /var/www/Raspcontrol && sudo mv /var/www/RaspcontrolUpdate /var/www/Raspcontrol', $retval);
+// Printing additional info
+echo '
+</pre>'; ?>
+Raspcontrol updated!<br/>
+<a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Return To Previous Page</a>
