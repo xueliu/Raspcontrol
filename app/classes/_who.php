8eebf553817cf9edb69f5a35ca5df3fdc38ebca9
Jacob Clark <jacob.jh.clark@googlemail.com>
2012-08-07 20:01:23 +0000

    Styled Active Users

--- app/classes/_who.php
+++ app/classes/_who.php
@@ -17,6 +17,7 @@ class usersLoggedIn{
 		  </div>
 		  
 		  <div class="userText">
+		  	<div style="width: 500px">
 		  	<?php if($whoUsersFormatted == ""){
 		  		echo "<strong>No users logged in</strong>";
 		  	}else{
@@ -35,19 +36,21 @@ class usersLoggedIn{
 						//Now split fields into multiple values
 						$fields = explode(" ", $line);
 					
-						$s .= "<strong>User:</strong> " . $fields[0] . "<br />";
+						$s .= "<div style='float: left; padding-bottom: 30px; padding-right: 20px;'><strong>User:</strong> " . $fields[0] . "<br />";
 						$s .= "<strong>TTY:</strong> " . $fields[1] . "<br />";
 						$s .= "<strong>From:</strong> " . $fields[2] . "<br />";
-						$s .= "<strong>Login @:</strong> " . $fields[3] . "<br />";
+						$s .= "<strong>Login @:</strong> " . $fields[3] . "</div>";
 
-						$s .= "<br /><br />";
+						$s .= "";
 					}
 				}
 				echo $s;
 		  	}
 			?>
+		  	</div>		  	
 		  </div>
 	
+	<div style="clear:both"></div>
 	
 	
 <?php
