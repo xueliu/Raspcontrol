643151ad89ec9da19d97a45f60da6f321cef5b32
Nicolas <github@nicolabricot.com>
2013-04-10 00:02:12 +0200

    Fix design alignement

--- app/pages/home.php
+++ app/pages/home.php
@@ -13,31 +13,27 @@ $ram_used = $ram['used'];
 $ram_total = $ram['total'];
 
 function icon_alert($alert) {
-  echo '<i class="icon-'. ($alert == 'danger' ? 'ok' : 'warning') .'"></i>';
+  echo '<i class="icon-'. ($alert == 'success' ? 'ok' : 'warning-sign') .'"></i>';
 }
 
 ?>
 
-      <div class="container">
-        <div class="row-fluid">
-          <div class="span10 offset1">
-            <table>
-              <tr>
-                <td><i class="icon-time"></i> Uptime</td>
-                <td></td>
-                <td><?php echo $uptime; ?></td>
-              </tr>
-              <tr>
-                <td><i class="icon-asterisk"></i> RAM</td>
-                <td><?php echo icon_alert($ram_alert); ?></td>
-                <td>
-                  <div class="progress">
-                    <div class="bar bar-<?php echo $ram_alert; ?>" style="width: <?php echo $ram_per; ?>%;"><?php echo $ram_per; ?>%</div>
-                  </div>
-                  free: <?php echo $ram_free; ?> Mb, used: <?php echo $ram_used; ?> Mb, total: <?php echo $ram_total; ?> Mb
-                </td>
-              </tr>
-            </table>
-          </div>
-        </div>
+      <div class="container home">
+        <table>
+          <tr>
+            <td><i class="icon-time"></i> Uptime</td>
+            <td class="icon"></td>
+            <td><?php echo $uptime; ?></td>
+          </tr>
+          <tr>
+            <td><i class="icon-asterisk"></i> RAM</td>
+            <td class="icon"><?php echo icon_alert($ram_alert); ?></td>
+            <td>
+              <div class="progress">
+                <div class="bar bar-<?php echo $ram_alert; ?>" style="width: <?php echo $ram_per; ?>%;"><?php echo $ram_per; ?>%</div>
+              </div>
+              free: <?php echo $ram_free; ?> Mb, used: <?php echo $ram_used; ?> Mb, total: <?php echo $ram_total; ?> Mb
+            </td>
+          </tr>
+        </table>
       </div>
