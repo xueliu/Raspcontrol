b80353766506ff467ebbf101638d6483d42f7d14
Nicolas <github@nicolabricot.com>
2013-04-09 22:40:29 +0200

    Try to display uptime

--- app/pages/home.php
+++ app/pages/home.php
@@ -1,7 +1,14 @@
+<?php
+
+namespace lib;
+use lib\Uptime;
+
+?>
+
       <div class="container">
         <div class="row">
-          <div class="span6 offset3">
-            <a href="<?php echo LOGIN; ?>?logout">Logout</a>
+          <div class="span6">
+            Uptime: <?php echo Uptime::uptime(); ?>
           </div>
         </div>
       </div>
