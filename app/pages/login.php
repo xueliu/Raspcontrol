53e4f38fbecad01a034b02aea0690769f225e46c
nicolabricot <github@nicolabricot.com>
2013-04-10 00:30:30 +0200

    Add autofocus in username field

--- app/pages/login.php
+++ app/pages/login.php
@@ -1,7 +1,7 @@
       <div class="container">
         <div class="center">
 			<form class="form-inline form-login" method="post" action="<?php echo LOGIN; ?>">
-			  <input type="text" name="username" id="username" placeholder="Username" />
+			  <input type="text" name="username" id="username" placeholder="Username" autofocus />
 			  <input type="password" name="password" id="password" placeholder="Password" />
 			  <input type="submit" class="btn btn-inverse" value="Connect" />
 			</form>
