5f97ed181fb8314785e6a778b3c1c3257a710a8b
nicolabricot <github@nicolabricot.com>
2013-04-09 15:19:49 +0200

    Update presentation

--- app/pages/home.php
+++ app/pages/home.php
@@ -1,12 +1,7 @@
     <div class="container">
       <div class="row">
-        <div class="span6">
-          <form action="./" method="post">
-          </form>
-        </div>
-
-        <div class="span6 center">
-          <a class="twitter-timeline" href="https://twitter.com/rbpi_bot"  data-widget-id="321548487399444480">@rbpi_bot</a>
+        <div class="span6 offset3">
+          <a href="<?php echo LOGIN; ?>?logout">Logout</a>
         </div>
       </div>
     </div> <!-- container -->
