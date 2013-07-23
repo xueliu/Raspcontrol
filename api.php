371ade028b198ed7ff42a3194af6dd43545ed3ae
Karl Woditsch <k.woditsch@gmail.com>
2013-07-23 16:29:05 +0200

    Add partial responses

--- api.php
+++ api.php
@@ -15,6 +15,47 @@ spl_autoload_register();
 
 require 'config.php';
 
+function build_rbpi($response){
+  $response['rbpi']['hostname'] = Rbpi::hostname(true);
+  $response['rbpi']['distribution'] = Rbpi::distribution();
+  $response['rbpi']['kernel'] = Rbpi::kernel();
+  $response['rbpi']['firmware'] = Rbpi::firmware();
+  $response['rbpi']['ip']['internal'] = Rbpi::internalIp();
+  $response['rbpi']['ip']['external'] = Rbpi::externalIp();
+  return $response;
+}
+function build_uptime($response){
+  $response['uptime'] = Uptime::uptime();
+  return $response;
+}
+function build_memory($response){
+  $response['memory']['ram'] = Memory::ram();
+  $response['memory']['swap'] = Memory::swap();
+  return $response;
+}
+function build_cpu($response){
+  $response['cpu']['usage'] = CPU::cpu();
+  $response['cpu']['heat'] = CPU::heat();
+  return $response;
+}
+function build_hdd($response){
+  $response['hdd'] = Storage::hdd();
+  return $response;
+}
+function build_net($response){
+  $response['net']['connections'] = Network::connections();
+  $response['net']['ethernet'] = Network::ethernet();
+  return $response;
+}
+function build_users($response){
+  $response['users'] = Users::connected();
+  return $response;
+}
+function build_services($response){
+  $response['services'] = Services::services();
+  return $response;
+}
+
 $result = array();
 
 try {
@@ -27,22 +68,47 @@ try {
     if(!empty($_GET['data'])){
       switch($_GET['data']){
         case 'all':
-          $result['rbpi']['hostname'] = Rbpi::hostname(true);
-          $result['rbpi']['distribution'] = Rbpi::distribution();
-          $result['rbpi']['kernel'] = Rbpi::kernel();
-          $result['rbpi']['firmware'] = Rbpi::firmware();
-          $result['rbpi']['ip']['internal'] = Rbpi::internalIp();
-          $result['rbpi']['ip']['external'] = Rbpi::externalIp();
-          $result['uptime'] = Uptime::uptime();
-          $result['memory']['ram'] = Memory::ram();
-          $result['memory']['swap'] = Memory::swap();
-          $result['cpu']['usage'] = CPU::cpu();
-          $result['cpu']['heat'] = CPU::heat();
-          $result['hdd'] = Storage::hdd();
-          $result['net']['connections'] = Network::connections();
-          $result['net']['ethernet'] = Network::ethernet();
-          $result['users'] = Users::connected();;
-          $result['services'] = Services::services();
+          $result = build_rbpi($result);
+          $result = build_uptime($result);
+          $result = build_memory($result);
+          $result = build_cpu($result);
+          $result = build_hdd($result);
+          $result = build_net($result);
+          $result = build_users($result);
+          $result = build_services($result);
+        break;
+        case 'rbpi':
+          $result = build_rbpi($result);
+        break;
+        case 'uptime':
+          $result = build_uptime($result);
+        break;
+        case 'memory':
+          $result = build_memory($result);
+        break;
+        case 'cpu':
+          $result = build_cpu($result);
+        break;
+        case 'hdd':
+          $result = build_hdd($result);
+        break;
+        case 'net':
+          $result = build_net($result);
+        break;
+        case 'users':
+          $result = build_users($result);
+        break;
+        case 'services':
+          $result = build_services($result);
+        break;
+        case 'details':
+          $result = build_rbpi($result);
+          $result = build_uptime($result);
+          $result = build_memory($result);
+          $result = build_cpu($result);
+          $result = build_hdd($result);
+          $result = build_net($result);
+          $result = build_users($result);
         break;
         default:
           $result['error'] = 'Incorrect data request.'; 
