3a8f30c2d0aad4e3ec89873b1e5e8b93f1328cba
Karl Woditsch <k.woditsch@gmail.com>
2013-07-23 09:48:16 +0200

    Improve status label

--- pages/services.php
+++ pages/services.php
@@ -5,6 +5,31 @@ use lib\Services;
 
 $services = Services::services();
 
+function label_service($status) {
+  echo '<span class="label label-';
+  switch($status) {
+    case '+':
+      echo 'success';
+      break;
+    case '?':
+      echo 'warning';
+      break;
+    default:
+      echo 'important';
+  }
+  echo '">';
+  switch($status) {
+    case '+':
+      echo 'Running';
+      break;
+    case '?':
+      echo 'Unknown';
+      break;
+    default:
+      echo 'Stopped';
+  }
+  echo '</span>';
+}
 
 ?>
 
@@ -14,7 +39,7 @@ $services = Services::services();
       <td class="check" rowspan="<?php echo sizeof($services); ?>"><i class="icon-cog"></i> Services</td>
       <?php
         for ($i = 0; $i < sizeof($services); $i++) {
-          echo '<td class="icon" style="padding-left: 10px;">', $services[$i]['status'], '</td>
+          echo '<td class="icon" style="padding-left: 10px;">', label_service($services[$i]['status']), '</td>
             <td class="infos">', $services[$i]['name'] , '</td>
           </tr>
           ', ($i == sizeof($hdd)-1) ? null : '<tr class="service">';
