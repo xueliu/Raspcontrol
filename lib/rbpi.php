70db1fb785e6eaeb572248a1d10f26b10b12d7bd
Harry Chen <373685963@qq.com>
2013-07-26 15:42:52 +0800

    Try another way to get external IP when fails.
    Signed-off-by: Harry Chen <373685963@qq.com>

--- lib/rbpi.php
+++ lib/rbpi.php
@@ -28,18 +28,40 @@ class Rbpi {
     return $_SERVER['SERVER_ADDR'];
   }
 
-  public static function externalIp() {
+  public static function externalIpByJson() {
     if (!function_exists('file_get_contents'))
-      return 'Unavailable';
+      return FALSE;
     $ip_json = file_get_contents('http://pv.sohu.com/cityjson?ie=utf-8');
     if ($ip_json==FALSE)
-      return 'Unavailable';
+      return FALSE;
     $ip_json = trim($ip_json);
     $ip_json = substr($ip_json, 19);
     $ip_json = substr($ip_json, 0, -1);
     $ip_arr = json_decode($ip_json,true);
     return $ip_arr['cip'];
   }
+
+  public static function externalIpByCurl() {
+    if(!function_exists('curl_init'))
+      return FALSE;
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://ifconfig.me');
+    curl_setopt($curl, CURLOPT_HEADER, 1);
+    $ip = curl_exec($curl);
+    curl_close($curl);
+    if($ip=='')
+      return FALSE;
+    return $ip;
+  }
+
+  public static function externalIp() {
+    $ip = self::externalIpByCurl();
+    if($ip==FALSE)
+      $ip = self::externalIpByJson();
+    if($ip==FALSE)
+      return 'Unavailable';
+    return $ip;
+}
 
   public static function webServer() {
     return$_SERVER['SERVER_SOFTWARE'];
